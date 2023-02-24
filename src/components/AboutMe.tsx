755a6ef00560e435cf5965e3caba721ecc67a68f
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-02-24 15:18:19 +0100

    :lipstick: add style to about section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,26 +3,29 @@ import Title from "./Title";
 const AboutMe = () => {
   return (
     <section className='section-aboutMe'>
-      <img
-        className='section-aboutMe-img'
-        src='./assets/images/profil-photo.png'
-        alt="C'est moi, Cécilia"
-      />
       <div className='section-aboutMe-content'>
-        <Title
-          text='DÉVELOPPEUR 
+        <img
+          className='section-aboutMe-img'
+          src='./assets/images/profil-photo.png'
+          alt="C'est moi, Cécilia"
+        />
+        <div className='section-aboutMe-text'>
+          <Title
+            text='DÉVELOPPEUSE 
                 FRONT END BASÉE 
                 À BORDEAUX'
-          numberTitle={1}
-        />
-        <p className='section-aboutMe-text'>
-          En tant que manager pendant des années, j'ai appris à être méthodique,
-          travailler en équipe et à être polyvalente. J'ai alors décidé de faire
-          passer ma passion en premier en devenant Développeur Front-End. Après
-          avoir suivi une formation diplômante, j'ai pu mettre mes compétences
-          en pratique à travers divers projets. Je suis maintenant motivée à
-          utiliser mes connaissances dans une nouvelle aventure.
-        </p>
+            numberTitle={1}
+          />
+          <p className='section-aboutMe-paragraph'>
+            En tant que manager pendant des années, j'ai appris à être
+            méthodique, travailler en équipe et à être polyvalente. J'ai alors
+            décidé de faire passer ma passion en premier en devenant Développeur
+            Front-End. Après avoir suivi une formation diplômante, j'ai pu
+            mettre mes compétences en pratique à travers divers projets. Je suis
+            maintenant motivée à utiliser mes connaissances dans une nouvelle
+            aventure.
+          </p>
+        </div>
       </div>
     </section>
   );
