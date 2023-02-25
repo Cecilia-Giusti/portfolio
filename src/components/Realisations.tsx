c221da0ce34123101ce43a0b62af0f219339fa97
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-02-25 10:28:22 +0100

    :construction: add realisations section

---
+++ src/components/Realisations.tsx
@@ -0,0 +1,13 @@
+import Title from "./Title";
+
+const Realisations = () => {
+  return (
+    <section className='section-realisations'>
+      <div className='section-realisations-content'>
+        <Title text='RÉALISATIONS' numberTitle={2} />
+      </div>
+    </section>
+  );
+};
+
+export default Realisations;
