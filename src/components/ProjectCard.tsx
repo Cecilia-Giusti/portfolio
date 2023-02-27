13af0da50f55f24fdd906672000c3835a4beda9a
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-02-27 15:02:40 +0100

    :lipstick: add style to project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,18 +8,27 @@ interface projetCardInt {
 const ProjetCard = ({ name, img, description, stack }: projetCardInt) => {
   return (
     <article className='article-card'>
-      <p className='article-card-article'>{name}</p>
+      <p className='article-card-title'>{name}</p>
       <img
         src={img}
         alt='Mockup de la maquette de Kasa'
         className='article-card-img'
       />
       <ul className='article-card-list'>
-        {stack.map((item, id) => (
-          <li key={id} className='article-card-item'>
-            {item}
-          </li>
-        ))}
+        {stack.map((item, id) => {
+          if (id === 0) {
+            return (
+              <li key={id} className='article-card-item'>
+                {item}
+              </li>
+            );
+          }
+          return (
+            <li key={id} className='article-card-item'>
+              | {item}
+            </li>
+          );
+        })}
       </ul>
       <p className='article-card-description'>{description}</p>
     </article>
