23af21947c0d742fba45c583aa4c9e1063467440
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-02-27 15:06:10 +0100

    :lipstick: add style to section-realisations

--- src/components/Realisations.tsx
+++ src/components/Realisations.tsx
@@ -6,25 +6,23 @@ import { Link } from "react-router-dom";
 const Realisations = () => {
   return (
     <section className='section-realisations'>
+      <Title text='RÉALISATIONS' numberTitle={2} />
       <div className='section-realisations-content'>
-        <Title text='RÉALISATIONS' numberTitle={2} />
-        <div>
-          {projects.projects.map((projet, id) => {
-            return (
-              <Link
-                to={`/mes-projets/${projet.name}`}
-                key={id}
-                className='section-realisations-link'>
-                <ProjectCard
-                  name={projet.name}
-                  img={projet.img}
-                  description={projet.description}
-                  stack={projet.stack}
-                />
-              </Link>
-            );
-          })}
-        </div>
+        {projects.projects.map((projet, id) => {
+          return (
+            <Link
+              to={`/mes-projets/${projet.name}`}
+              key={id}
+              className='section-realisations-link'>
+              <ProjectCard
+                name={projet.name}
+                img={projet.img}
+                description={projet.description}
+                stack={projet.stack}
+              />
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
