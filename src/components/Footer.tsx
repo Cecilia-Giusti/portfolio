4d829be9c85ec57f4c150379bd503d82f138b236
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-02-27 13:45:49 +0100

    :lipstick: add style to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,29 @@
+import ButtonCallToAction from "./ButtonCallToAction";
+import socialIcons from "../data/database.json";
+import SocialNetwork from "./SocialNetwork";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className='footer'>
+      <p className='footer-text'>À bientôt ! </p>
+      <div className='footer-callToAction'>
+        <ul className='footer-list'>
+          {socialIcons.socialNetwork.map((item, id) => {
+            return (
+              <li className='footer-item' key={id}>
+                <SocialNetwork url={item.url} logo={item.logo} />
+              </li>
+            );
+          })}
+        </ul>
+
+        <ButtonCallToAction text='Contactez-moi' url='/' />
+      </div>
+      <button className='footer-button-top'>
+        <i className='fa-solid fa-chevron-up'></i>
+      </button>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/SocialNetwork.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+interface socialNetworkInt {
+  url: string;
+  logo: string;
+}
+
+const SocialNetwork = ({ url, logo }: socialNetworkInt) => {
+  return (
+    <Link to={url} className='footer-item-link'>
+      <button className='footer-item-button'>
+        <i className={logo}></i>
+      </button>
+    </Link>
+  );
+};
+
+export default SocialNetwork;
