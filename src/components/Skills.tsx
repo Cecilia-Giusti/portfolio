8bca39c7dbb1c1bbd5294f70e0fe7b2854250f23
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2023-06-07 10:03:14 +0200

    :package: Add dockerfile

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -16,18 +16,18 @@ const Skills = forwardRef<HTMLDivElement, skillsInt>((props, ref) => {
           <p className='section-skills-text'>
             Je suis passionnée par la création de solutions fiables et
             intuitives pour les utilisateurs. Je suis capable d'intégrer des
-            maquettes en utilisant React/Vue et en respectant les principes de
-            l'Atomic Design. Je suis également à l'aise pour résoudre des
-            problèmes qui peuvent survenir au cours d'un projet. Je comprends
-            l'importance d'avoir un code propre, robuste et maintenable, c'est
-            pourquoi j'écris des tests pour garantir une couverture cohérente
-            avec le projet et identifier les bugs de manière proactive.
-            J'apprécie de travailler avec des outils qui m'aident à être
-            organisée, mais je suis également ouverte à apprendre de nouveaux
-            outils qui peuvent améliorer mon travail. En somme, je suis une
-            développeuse déterminée à livrer des projets de qualité, dans les
-            délais impartis, tout en garantissant un investissement optimal au
-            le client.
+            maquettes et créer des applications en utilisant React/TS tout en
+            respectant les principes de l'Atomic Design. Je suis également à
+            l'aise pour résoudre des problèmes qui peuvent survenir au cours
+            d'un projet. Je comprends l'importance d'avoir un code propre,
+            robuste et maintenable, c'est pourquoi j'écris des tests pour
+            garantir une couverture cohérente avec le projet et identifier les
+            bugs de manière proactive. J'apprécie de travailler avec des outils
+            qui m'aident à être organisée, mais je suis également ouverte à
+            apprendre de nouveaux outils qui peuvent améliorer mon travail. En
+            somme, je suis une développeuse déterminée à livrer des projets de
+            qualité, dans les délais impartis, tout en garantissant un
+            investissement optimal avec toutes les parties prenantes du projet.
           </p>
           <div className='section-skills-card'>
             <ul className='section-skills-list'>
